b857a01384024e718265335eeddba872d62f75a6
Christian Alvarado <tianqueal@gmail.com>
2026-08-07 13:21:44 +0200

    fix: menu transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,14 @@ return {
     opts = {
       style = "moon",
       light_style = "day",
+
+      on_colors = function(colors)
+        colors.bg_statusline = colors.none
+      end,
+
+      -- on_highlights = function(hl, c)
+      --  hl.AvanteSidebarWinSeparator = { link = "WinSeparator" }
+      -- end,
     },
     config = function(_, opts)
       require("tokyonight").setup(opts)
